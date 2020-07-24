bd7d9006389fbf32ca2150daf4becb8db649acd8
Ron Brogan <ronaldbrogan@hotmail.com>
2020-07-24 00:48:44 -0700

    Change config root environment variable to accomodate tooling that restricts hyphens

--- src/OpenH2.Core/Configuration/ConfigurationConstants.cs
+++ src/OpenH2.Core/Configuration/ConfigurationConstants.cs
@@ -6,7 +6,7 @@ namespace OpenH2.Core.Configuration
 {
     public static class ConfigurationConstants
     {
-        public const string ConfigPathOverrideEnvironmentVariable = "openh2--configroot";
+        public const string ConfigPathOverrideEnvironmentVariable = "openh2_configroot";
         public const string MaterialConfigName = "material-config.json";
     }
 }
